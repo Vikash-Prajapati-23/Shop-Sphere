ad7bfd7e0097c6907422c7dd204e0f1f5d1f78ca
Vikash-Prajapati-23 <122448220+Vikash-Prajapati-23@users.noreply.github.com>
2024-12-03 02:12:19 +0530

    ...

--- Frontend/shop_sphare/src/components/WishList/WishList.js
+++ Frontend/shop_sphare/src/components/WishList/WishList.js
@@ -1,15 +1,20 @@
-import React from 'react'
-import './Style/WishList.css'
+import React from "react";
+import "./Style/WishList.css";
+import Card from '../Card/Card'
 
-const WishList = ({onClick}) => {
+const WishList = ({ onClick, getData, addToWishlist }) => {
   return (
-    <div className='card m-3'>
+    <div className="card m-3">
       <h3>Your Wishlist</h3>
       {/* <span onClick={onClick}>&#10084;&#65039;</span> */}
 
+      <Card
+        addToWishlist
+      />
+
       <div className="wishLists card m-3" onClick={onClick}></div>
     </div>
-  )
-}
+  );
+};
 
-export default WishList
+export default WishList;
