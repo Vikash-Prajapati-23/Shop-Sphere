e220d55740769f5b4c24e273f60199eb70f787af
Vikash-Prajapati-23 <122448220+Vikash-Prajapati-23@users.noreply.github.com>
2025-02-16 18:19:53 +0530

    Add to cart all set.!

--- Frontend/shop_sphare/src/components/Cart/Cart.jsx
+++ Frontend/shop_sphare/src/components/Cart/Cart.jsx
@@ -5,34 +5,53 @@ import Button from '../Button/Button';
 const Cart = ({ cart }) => {
   return (
     <div className='container bg-clr my-5'>
-      <h2 className='text-center my-4'>Shopping Cart</h2>
+      <h2 className='text-center py-4'>Shopping Cart</h2>
       <ul >
-        {cart.length === 0 ? <p>Cart is empty</p> : (
+        {cart.length === 0 ? <div className='d-flex align-items-center justify-content-center gap-5 py-4'> <img src='./images/empty-cart.png' ></img> <h4> Your cart is empty.!</h4> </div> : (
           <div>
             {cart.map((item) => (
-              <li className='cart-list rounded m-2' key={item.id} >
+              <li className='cart-container cart-list rounded m-2 py-3' key={item.id} >
+
                 <img src={item.image} className='cart-img cart-item mx-5' alt={item.title} />
-                <div className=' container '>
-                  <div className=' d-flex justify-content-between '>
-                    <div className='cart-item mx-5'>{item.title ? item.title.slice(0, 45) : "No Title"}...</div>
-                    <div className='cart-item me-5 '> - </div>
+
+                <div className=' d-flex justify-content-between align-items-center'>
+
+                  <div className='   justify-content-between '>
+                    <pre className='cart-item '>{item.title.slice(0, 40) || "No Title"}...</pre>
+                    <div className=' d-flex align-items-center'>
+                      <p className="">
+                        <span className='fw-bold' style={{ color: "gold" }}>★ </span>
+                        <span className='me-3' >{item.rating.rate}</span>
+                      </p>
+                      <p className="fw-bold">Reviews {item.rating.count}</p>
+                    </div>
+                    <div className=' d-flex justify-content-between align-items-center'>
+                      <Button className='btn btn-danger fw-bold' btnName={"-"} />
+                      <div className='d-flex align-items-center px-3 fw-bold' >{item.quantity}</div>
+                      <Button className='btn btn-success fw-bold me-3' btnName={"+"} />
+                      <Button className='btn btn-success fw-bold ' btnName={"Move to wishlist"} />
+                    </div>
+                  </div>
+
+                  <div className=' ms-5 align-items-center'>
                     <div className='cart-item me-5'>${item.price} (x{item.quantity})</div>
+
+                  </div>
+
+                  <div className=' ms-5 align-items-center'>
                     <button className='btn text-danger ' >
                       <span className="material-symbols-outlined">
                         delete
                       </span>
                     </button>
                   </div>
-                  <div className='d-flex ms-5'>
-                    <Button className='btn btn-danger fw-bold' btnName={"-"} />
-                    <div className='d-flex align-items-center px-3' >{item.quantity}</div>
-                    <Button className='btn btn-success fw-bold' btnName={"+"} />
-                  </div>
+
                 </div>
               </li>
             ))}
-            <div className='cart-total'>
-              <span className='d-flex justify-content-end py-3'>Total: ${cart.reduce((total, item) => total + item.price * item.quantity, 0)}</span>
+            <div className='cart-total d-flex justify-content-end'>
+              <span className=' mx-3 py-3'>Total: ${cart.reduce((total, item) => total + item.price * item.quantity, 0)}</span>
+              <Button className='btn btn-primary fw-bold' btnName={"Place Order"} />
             </div>
           </div>
         )}
