3dfb4700065558dbb9d0e63ab586fe4afb74ab69
Vikash-Prajapati-23 <122448220+Vikash-Prajapati-23@users.noreply.github.com>
2025-06-21 16:08:15 +0530

    ...

--- Backend/controllers/cartController.js
+++ Backend/controllers/cartController.js
@@ -4,15 +4,20 @@ import mongoose from "mongoose";
 
 export async function getCart(req, res) {
   try {
-    const userId = req.user.id; // Assuming `req.user`
-    // Populate productId to get full product details
+    const userId = req.user.id;
     const populatedCart = await Cart.find({ userId }).populate("productId");
-    // Include quantity in the response
-    const populatedProducts = populatedCart.filter((item => item.productId).map(item => ({
-      ...item.productId.toObject(),
-      quantity: item.quantity,
-    })))
-    // Return the populated products along with the cart items.
+
+    if (!populatedCart || populatedCart.length === 0) {
+      return res.status(200).json([]); // Return empty cart if nothing found
+    }
+
+    const populatedProducts = populatedCart
+      .filter((item) => item?.productId)
+      .map((item) => ({
+        ...item.productId.toObject(),
+        quantity: item.quantity,
+      }));
+
     return res.status(200).json(populatedProducts);
   } catch (error) {
     console.error("Error fetching cart:", error);
@@ -48,7 +53,7 @@ export async function addToCart(req, res) {
 
     // Add to cart with quantity initialized to 1
     const cartItem = await Cart.create({ userId, productId, quantity: 1 });
-    return res.status(201).json({cartItem, message: "Product added to cart" });
+    return res.status(201).json({ cartItem, message: "Product added to cart" });
   } catch (error) {
     console.error("Error adding to cart:", error);
     return res.status(500).json({ message: "Internal Server Error" });
@@ -84,8 +89,6 @@ export async function productDecrement(req, res) {
   try {
     const userId = req.user.id;
     const { productId } = req.params;
-
-
 
     if (!mongoose.Types.ObjectId.isValid(productId)) {
       return res.status(400).json({ message: "Invalid product ID" });
