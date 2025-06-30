6054bb1ddf9104cc411c5c4eb834cfc15963a803
Vikash-Prajapati-23 <122448220+Vikash-Prajapati-23@users.noreply.github.com>
2025-07-01 00:32:39 +0530

    Almost complete.!

--- Frontend/src/components/Dashboard/Profile/Profile.jsx
+++ Frontend/src/components/Dashboard/Profile/Profile.jsx
@@ -11,6 +11,7 @@ const Profile = ({ name, setIsLoggedIn }) => {
     location.state?.section === "manageAddresses" ? 2 : 1
   );
   const navigate = useNavigate();
+  const [isOpen, setIsOpen] = useState(false);
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
@@ -50,89 +51,95 @@ const Profile = ({ name, setIsLoggedIn }) => {
 
   return (
     <div className="Profile">
-      <aside className="aside-user ">
-        <div className="user mb-3">
-          <div>
-            <img
-              className="img-user"
-              src="/images/User-Profile-PNG-Image.png"
-              alt="User"
-            />
-          </div>
-          <div className="user-name">
-            <p className="hello profile-text-size-s">Hello,</p>
-            <p className="name-text profile-text-size-b">
-              {name
-                ? name.slice(0, 1).toUpperCase() + name.slice(1)
-                : formData.firstName}
-            </p>
-          </div>
-        </div>
-
-        <div className="side-profile">
-          <Link className="text-decoration-none" to={"/MyOrders"}>
-            <div className="d-flex side-profile-secs">
-              <i className="fas fa-box mt-1 me-lg-3 me-2 text-primary profile-text-size-s"></i>
-              <div className="d-flex">
-                <h5 className="aside-heads profile-text-size-b me-lg-5 me-md-4">
-                  My orders
-                </h5>
-                <i className="bi bi-chevron-right text-dark profile-text-size-s"></i>
-              </div>
+      {!isOpen ? (
+        <button onClick={() => setIsOpen(true)} className="side-nav-icon">
+          <i class="fa-solid fa-bars"></i>
+        </button>
+      ) : (
+        <aside className="aside-user ">
+          <div className="user mb-3">
+            <div>
+              <img
+                className="img-user"
+                src="/images/User-Profile-PNG-Image.png"
+                alt="User"
+              />
             </div>
-          </Link>
+            <div className="user-name">
+              <p className="hello profile-text-size-s">Hello,</p>
+              <p className="name-text profile-text-size-b">
+                {name
+                  ? name.slice(0, 1).toUpperCase() + name.slice(1)
+                  : formData.firstName}
+              </p>
+            </div>
+          </div>
 
-          <div className="side-profile-secs">
-            <div className="d-flex">
-              <i className="fas fa-user-circle me-lg-3 me-2 mt-1 text-primary profile-text-size-s"></i>
-              <h5 className="aside-heads profile-text-size-b">
-                Account settings
+          <div className="side-profile">
+            <Link className="text-decoration-none" to={"/MyOrders"}>
+              <div className="d-flex side-profile-secs">
+                <i className="fas fa-box mt-1 me-lg-3 me-2 text-primary profile-text-size-s"></i>
+                <div className="d-flex">
+                  <h5 className="aside-heads profile-text-size-b me-lg-5 me-md-4">
+                    My orders
+                  </h5>
+                  <i className="bi bi-chevron-right text-dark profile-text-size-s"></i>
+                </div>
+              </div>
+            </Link>
+
+            <div className="side-profile-secs">
+              <div className="d-flex">
+                <i className="fas fa-user-circle me-lg-3 me-2 mt-1 text-primary profile-text-size-s"></i>
+                <h5 className="aside-heads profile-text-size-b">
+                  Account settings
+                </h5>
+              </div>
+              <ul className="mb-0 ps-md-4 profile-text-size-s">
+                <li
+                  onClick={() => {
+                    if (currentIndex > 1) setCurrentIndex(currentIndex - 1);
+                  }}
+                  className={`lists ${currentIndex === 1 ? "disabled" : ""}`}
+                >
+                  Personal information
+                </li>
+                <li
+                  onClick={() => {
+                    if (currentIndex === 1) setCurrentIndex(currentIndex + 1);
+                  }}
+                  className={`lists ${currentIndex === 2 ? "disabled" : ""}`}
+                >
+                  Manage addresses
+                </li>
+              </ul>
+            </div>
+
+            <div className=" side-profile-secs">
+              <div className="d-flex">
+                <i className="fas fa-wallet me-lg-3 me-2 mt-1 text-primary profile-text-size-s"></i>
+                <h5 className="aside-heads profile-text-size-b">Payments</h5>
+              </div>
+
+              <ul className="mb-0 ps-md-4 profile-text-size-s">
+                <li className="lists">Gift Cards</li>
+                <li className="lists">Saved UPI</li>
+                <li className="lists">Saved Cards</li>
+              </ul>
+            </div>
+
+            <div className="d-flex justify-content-start side-profile-secs cursor text-primary">
+              <i className="ri-logout-circle-r-line user-icon me-lg-3 me-2 profile-text-size-s"></i>
+              <h5
+                onClick={handleProfileLogout}
+                className="aside-heads profile-text-size-b"
+              >
+                Log Out
               </h5>
             </div>
-            <ul className="mb-0 ps-md-4 profile-text-size-s">
-              <li
-                onClick={() => {
-                  if (currentIndex > 1) setCurrentIndex(currentIndex - 1);
-                }}
-                className={`lists ${currentIndex === 1 ? "disabled" : ""}`}
-              >
-                Personal information
-              </li>
-              <li
-                onClick={() => {
-                  if (currentIndex === 1) setCurrentIndex(currentIndex + 1);
-                }}
-                className={`lists ${currentIndex === 2 ? "disabled" : ""}`}
-              >
-                Manage addresses
-              </li>
-            </ul>
           </div>
-
-          <div className=" side-profile-secs">
-            <div className="d-flex">
-              <i className="fas fa-wallet me-lg-3 me-2 mt-1 text-primary profile-text-size-s"></i>
-              <h5 className="aside-heads profile-text-size-b">Payments</h5>
-            </div>
-
-            <ul className="mb-0 ps-md-4 profile-text-size-s">
-              <li className="lists">Gift Cards</li>
-              <li className="lists">Saved UPI</li>
-              <li className="lists">Saved Cards</li>
-            </ul>
-          </div>
-
-          <div className="d-flex justify-content-start side-profile-secs cursor text-primary">
-            <i className="ri-logout-circle-r-line user-icon me-lg-3 me-2 profile-text-size-s"></i>
-            <h5
-              onClick={handleProfileLogout}
-              className="aside-heads profile-text-size-b"
-            >
-              Log Out
-            </h5>
-          </div>
-        </div>
-      </aside>
+        </aside>
+      )}
 
       {currentIndex === 1 && (
         <PersonalInfo
