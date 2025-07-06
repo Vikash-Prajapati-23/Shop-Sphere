adf0da50dfa751b0c35f9fbcd5ec2fe9cea3fdbd
Vikash-Prajapati-23 <122448220+Vikash-Prajapati-23@users.noreply.github.com>
2025-07-07 00:28:43 +0530

    Testing.

--- Backend/index.js
+++ Backend/index.js
@@ -32,6 +32,13 @@ app.use(
     credentials: true,
   })
 );
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://shop-sphere-jade.vercel.app");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use(express.json());
 app.use(cookieParser());
 
